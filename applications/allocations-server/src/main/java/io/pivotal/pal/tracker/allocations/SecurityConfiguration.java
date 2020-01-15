348aec4538ffc0945792cd90b3ebe129b56608d7
sanjeetchoudhary <sanjeet.choudhary1@gmail.com>
2020-01-16 00:06:50 +0530

    cloudNativeDeveloperDistributedSystemWithServiceDiscovery changing  package correction securityconfiguration file

--- applications/allocations-server/src/main/java/io/pivotal/pal/tracker/allocations/SecurityConfiguration.java
+++ applications/allocations-server/src/main/java/io/pivotal/pal/tracker/allocations/SecurityConfiguration.java
@@ -1,4 +1,4 @@
-package io.pivotal.pal.tracker.registration;
+package io.pivotal.pal.tracker.allocations;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
